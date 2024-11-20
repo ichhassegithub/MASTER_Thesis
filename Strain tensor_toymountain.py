405fed9e8aafb443acfc1bd7b97bedca15cf4232
ichhassegithub <117379010+ichhassegithub@users.noreply.github.com>
2024-11-20 14:08:49 +0100

    Update Strain tensor_toymountain.py

--- Strain tensor_toymountain.py
+++ Strain tensor_toymountain.py
@@ -63,7 +63,7 @@ periodic_x = False
 periodic_y = False
 periodic_z = False
 periodic = [periodic_x, periodic_y, periodic_z]
-bool_unsteady = True
+bool_unsteady = False
 ## defining original meshgrid 
 X,Y,Z = np.meshgrid(x,y,zu_3d)
 dx_data = X[0,1,0]-X[0,0,0] 
